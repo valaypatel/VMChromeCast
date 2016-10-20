c7ba4ce9719e48fd300e6cbcdd7dbd74f45b7c87
Valay Patel <vpatel@schoolmessanger.com>
2016-10-20 01:47:37 -0400

    Video Playback is working

--- CastAnything/CastAnything/AppDelegate.swift
+++ CastAnything/CastAnything/AppDelegate.swift
@@ -2,43 +2,57 @@
 //  AppDelegate.swift
 //  CastAnything
 //
-//  Created by Valay Patel on 2016-06-30.
-//  Copyright © 2016 Valay Patel. All rights reserved.
+//  Created by Valay Patel on 2016-09-24.
+//  Copyright © 2016 FirstAim. All rights reserved.
 //
 
 import UIKit
+import GoogleCast
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate,GCKLoggerDelegate {
 
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let options = GCKCastOptions.init(receiverApplicationID: "4D07A748")
+        GCKCastContext.setSharedInstanceWith(options)
+        GCKCastContext.sharedInstance().useDefaultExpandedMediaControls = true
+        let logFilter = GCKLoggerFilter.init()
+        logFilter.exclusive = true
+        GCKLogger.sharedInstance().filter = logFilter
+        GCKLogger.sharedInstance().delegate = self
+
+        
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func logMessage(_ message: String, fromFunction function: String) {
+        print("\(function),\(message)")
     }
 
 
--- CastAnything/CastAnything/ViewController.swift
+++ CastAnything/CastAnything/ViewController.swift
@@ -2,24 +2,98 @@
 //  ViewController.swift
 //  CastAnything
 //
-//  Created by Valay Patel on 2016-06-30.
-//  Copyright © 2016 Valay Patel. All rights reserved.
+//  Created by Valay Patel on 2016-09-24.
+//  Copyright © 2016 FirstAim. All rights reserved.
 //
 
 import UIKit
+import GoogleCast
+import AVFoundation
+import AVKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,GCKSessionManagerListener,UIWebViewDelegate {
 
+    var castButton: GCKUICastButton?
+    @IBOutlet var webView: UIWebView?
+    private var castSessionManager: GCKSessionManager?
+    private var castSession: GCKCastSession?
+    
+//    override func loadView() {
+//        let webConfiguration = WKWebViewConfiguration()
+//        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+//        view = webView
+//    }
+    
+    func windowDidBecomeVisible(notification: NSNotification) {
+//        for mainWindow in UIApplication.shared.windows {
+//            for mainWindowSubview in mainWindow.subviews {
+//                // this will print:
+//                // 1: `WKWebView` + `[WKScrollView]`
+//                // 2: `UIView` + `[]`
+//                print("\(mainWindowSubview) \(mainWindowSubview.subviews)")
+//            }
+//        }
+        var sorted = UIApplication.shared.windows.sorted { (one, two) -> Bool in
+            return one.windowLevel == two.windowLevel
+        }
+        
+        let topWindow = sorted.first
+        let controller = topWindow?.rootViewController
+        let alert = UIAlertController.init(title: "Cast", message: "Do you want to cast Video", preferredStyle: .actionSheet)
+        controller?.dismiss(animated: true, completion: nil)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        castSessionManager = GCKCastContext.sharedInstance().sessionManager
+        castSessionManager?.add(self)
+        castButton = GCKUICastButton.init(frame: CGRect.init(x: 0, y: 0, width: 24, height: 24))
+        castButton?.tintColor = UIColor.blue
+        
+        if let castButton = castButton {
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(customView: castButton)
+        }
+        NotificationCenter.default.addObserver(self, selector: #selector(playerItemBecameCurrent(notifcation:)), name: NSNotification.Name(rawValue: "AVPlayerItemBecameCurrentNotification"), object: nil)
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(castDeviceDidChange(notification:)), name: NSNotification.Name.gckCastStateDidChange, object: GCKCastContext.sharedInstance())
+        
+        if let webView = webView {
+            if let url = URL.init(string: "https://youtube.com") {
+                let request = URLRequest.init(url: url)
+                webView.loadRequest(request)
+            }
+        }
+    }
+    
+    
+    func playerItemBecameCurrent(notifcation:NSNotification)
+    {
+        NotificationCenter.default.addObserver(self, selector: #selector(windowDidBecomeVisible), name: NSNotification.Name.UIWindowDidBecomeVisible, object: nil)
+        if let playerItem = notifcation.object as? AVPlayerItem {
+            if let asset = playerItem.asset as? AVURLAsset {
+                let _ = asset.url
+                
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isTranslucent = false
+    }
+    
+    func castDeviceDidChange(notification:Notification) {
+        
+    }
 
 }
 
